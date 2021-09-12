250f8b9bfb5d1e00687e1c3519d4135f598534c7
lichengzhen <1416311778@qq.com>
2021-09-12 19:33:38 +0800

    Initial Commit

---
+++ src/main/java/qst/git/Luck.java
@@ -0,0 +1,11 @@
+package qst.git;
+
+/**
+ * @author lcz
+ * @create 2021-09-12-19:24
+ */
+public class Luck {
+    public static void main(String[] args) {
+        System.out.println("Git Hello!");
+    }
+}
